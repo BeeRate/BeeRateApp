7141c14250ac41e18139ba49afe8049baa13a379
Sivan Tytiun <sivant@playtika.com>
2019-04-04 17:24:49 +0300

    fixed navigator

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
-import { createStackNavigator,createAppContainer } from 'react-navigation'
+import { createSwitchNavigator,createAppContainer } from 'react-navigation'
 
 import Loading from './src/components/Loading'
 import SignUp from './src/components/SignUp'
